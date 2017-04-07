cc6432132dc7952e398431fb32ba7b682f307c77
FlowerChat <flowerchatbot@gmail.com>
2017-04-07 21:11:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def makeWebhookResult(req):
         
         {
             "type": "link",
-            "url": "http://www.fiorita.cz"
+            "url": "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAb7Vnq1nSojwYd1TarHx_x6Gb4ti8bhVo"
         },
         {
             "type": "picture",
