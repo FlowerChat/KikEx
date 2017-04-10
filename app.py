770b7e0cb05029d313290d7fe57b77a5b18313b4
FlowerChat <flowerchatbot@gmail.com>
2017-04-10 10:00:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,8 @@ def makeWebhookResult(req):
         
         {
             "type": "picture",
-            "picUrl": "https://lh4.googleusercontent.com/-1wzlVdxiW14/USSFZnhNqxI/AAAAAAAABGw/YpdANqaoGh4/s1600-w400/Google%2BSydney"
-
-        },
-       # {
+            "picUrl": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyAb7Vnq1nSojwYd1TarHx_x6Gb4ti8bhVo"    
+            # {
        #     "type": "text",
        #     "body": photo_request
        # },
