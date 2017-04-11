64b20287a17dffb4958d661a3fa565b42d120466
FlowerChat <flowerchatbot@gmail.com>
2017-04-11 15:25:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ def makeWebhookResult(req):
     
     photo_payload = {"key" : key, "maxwidth": 1600, "maxhight": 1600, "photoreference" : photo_id}
     photo_request = requests.get(photos_url, params=photo_payload)
-    final_pic=str(photo_request)
+    #final_pic=str(photo_request)
     
-    #final_pic=photos_url+ques+photo_width+amp+photo_ref+photo_id+amp+key_eq+"AIzaSyAb7Vnq1nSojwYd1TarHx_x6Gb4ti8bhVo"
+    final_pic=photos_url+ques+photo_width+amp+photo_ref+photo_id+amp+key_eq+"AIzaSyAb7Vnq1nSojwYd1TarHx_x6Gb4ti8bhVo"
     
     
 
