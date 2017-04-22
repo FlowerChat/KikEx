599cd6a69da88d0a0c192d67ec97b0d9a6d9976d
FlowerChat <flowerchatbot@gmail.com>
2017-04-23 00:10:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def makeWebhookResult(req):
     #database filling
     db=psycopg2.connect(host="ec2-54-235-181-120.compute-1.amazonaws.com", dbname="dfgsds81qmj1m8", user="kvziloygxjkgdk", password="b47e268477aef14509ad98d05b99a7078d4a18bc82862b3796844fef65ea7367")
     cur=db.cursor()
-    cur.execute("INSERT INTO public.flowerchattable(flowerchattable.SessID, flowerchattable.TimeStamp) VALUES (ID, TimeStamp)")
+    cur.execute("INSERT INTO public.flowerchattable(sessid, timestamp) VALUES (ID, TimeStamp)")
     db.create_all()
 
     db.commit()
